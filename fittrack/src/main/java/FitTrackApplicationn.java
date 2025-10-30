fa4eaa69f2c0bf034ecab4af936b5df7fdc07677
Dinu Adhikari <dinuadhikari68@gmail.com>
2025-10-30 17:07:56 +0530

    Add User-Profile-Module enhancements

---
+++ fittrack/src/main/java/FitTrackApplicationn.java
@@ -0,0 +1,11 @@
+package com.fittrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackApplicationn {
+    public static void main(String[] args) {
+        SpringApplication.run(com.fittrack.FitTrackApplicationn.class, args);
+    }
+}
